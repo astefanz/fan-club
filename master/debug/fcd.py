42abb38169f71536639901873649c2180872308c
Alejandro Stefan Zavala <astefanz@berkeley.edu>
2019-01-12 20:13:04 -0800

    Added master preliminary build, test, and debug facilities for MkIV, as well as self-contained ARM GCC compiler

---
+++ master/debug/fcd.py
@@ -0,0 +1,57 @@
+################################################################################
+## Project: Fanclub Mark IV "Master" debug ## File: fcd.py                    ##
+##----------------------------------------------------------------------------##
+## CALIFORNIA INSTITUTE OF TECHNOLOGY ## GRADUATE AEROSPACE LABORATORY ##     ##
+## CENTER FOR AUTONOMOUS SYSTEMS AND TECHNOLOGIES                      ##     ##
+##----------------------------------------------------------------------------##
+##      ____      __      __  __      _____      __      __    __    ____     ##
+##     / __/|   _/ /|    / / / /|  _- __ __\    / /|    / /|  / /|  / _  \    ##
+##    / /_ |/  / /  /|  /  // /|/ / /|__| _|   / /|    / /|  / /|/ /   --||   ##
+##   / __/|/ _/    /|/ /   / /|/ / /|    __   / /|    / /|  / /|/ / _  \|/    ##
+##  / /|_|/ /  /  /|/ / // //|/ / /|__- / /  / /___  / -|_ - /|/ /     /|     ##
+## /_/|/   /_/ /_/|/ /_/ /_/|/ |\ ___--|_|  /_____/| |-___-_|/  /____-/|/     ##
+## |_|/    |_|/|_|/  |_|/|_|/   \|___|-    |_____|/   |___|     |____|/       ##
+##                   _ _    _    ___   _  _      __  __   __                  ##
+##                  | | |  | |  | T_| | || |    |  ||_ | | _|                 ##
+##                  | _ |  |T|  |  |  |  _|      ||   \\_//                   ##
+##                  || || |_ _| |_|_| |_| _|    |__|  |___|                   ##
+##                                                                            ##
+##----------------------------------------------------------------------------##
+## Alejandro A. Stefan Zavala ## <astefanz@berkeley.edu>   ##                 ##
+## Chris J. Dougherty         ## <cdougher@caltech.edu>    ##                 ##
+## Marcel Veismann            ## <mveisman@caltech.edu>    ##                 ##
+################################################################################
+
+""" ABOUT ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ + Custom debug facilities for the Fan Club Master-side.
+ +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ """
+
+## IMPORTS #####################################################################
+
+import inspect
+    # Print line numbers. See:
+    #   http://code.activestate.com/recipes/
+    #       145297-grabbing-the-current-line-number-easily/
+import sys
+
+## GLOBAL CONSTANTS ############################################################
+PREFIX = "[DEBUG]"
+FILE = sys.stdout
+
+## HELPER DEBUG FUNCTIONS ######################################################
+
+def ln():
+    """ Return the number of the line in source code in which this function is
+        called.
+    """
+    return inspect.currentframe().f_back.f_lineno
+
+def l(message = "", prefix = PREFIX, postfix = '\n'):
+    """ Print the number of the line in which the function is called.
+        - MESSAGE specifies an optional text to include when printing.
+          Defaults to an empty string.
+        - PREFIX defaults to the standard debug prefix string.
+        - POSTFIX defaults to newline.
+    """
+    print(prefix, "L:", inspect.currentframe().f_back.f_lineno, message,
+        end = postfix, file = FILE)
---
+++ master/test.py
@@ -0,0 +1,46 @@
+################################################################################
+## Project: Fanclub Mark IV "Master" main test suite ## File: test.py         ##
+##----------------------------------------------------------------------------##
+## CALIFORNIA INSTITUTE OF TECHNOLOGY ## GRADUATE AEROSPACE LABORATORY ##     ##
+## CENTER FOR AUTONOMOUS SYSTEMS AND TECHNOLOGIES                      ##     ##
+##----------------------------------------------------------------------------##
+##      ____      __      __  __      _____      __      __    __    ____     ##
+##     / __/|   _/ /|    / / / /|  _- __ __\    / /|    / /|  / /|  / _  \    ##
+##    / /_ |/  / /  /|  /  // /|/ / /|__| _|   / /|    / /|  / /|/ /   --||   ##
+##   / __/|/ _/    /|/ /   / /|/ / /|    __   / /|    / /|  / /|/ / _  \|/    ##
+##  / /|_|/ /  /  /|/ / // //|/ / /|__- / /  / /___  / -|_ - /|/ /     /|     ##
+## /_/|/   /_/ /_/|/ /_/ /_/|/ |\ ___--|_|  /_____/| |-___-_|/  /____-/|/     ##
+## |_|/    |_|/|_|/  |_|/|_|/   \|___|-    |_____|/   |___|     |____|/       ##
+##                   _ _    _    ___   _  _      __  __   __                  ##
+##                  | | |  | |  | T_| | || |    |  ||_ | | _|                 ##
+##                  | _ |  |T|  |  |  |  _|      ||   \\_//                   ##
+##                  || || |_ _| |_|_| |_| _|    |__|  |___|                   ##
+##                                                                            ##
+##----------------------------------------------------------------------------##
+## Alejandro A. Stefan Zavala ## <astefanz@berkeley.edu>   ##                 ##
+## Chris J. Dougherty         ## <cdougher@caltech.edu>    ##                 ##
+## Marcel Veismann            ## <mveisman@caltech.edu>    ##                 ##
+################################################################################
+
+""" ABOUT ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ + Unit tests for the FCMkIV main file.
+ +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ """
+
+## IMPORTS #####################################################################
+import unittest
+import traceback
+    # See: https://stackoverflow.com/questions/3702675/
+    #   how-to-print-the-full-traceback-without-halting-the-program
+import main
+
+## TEST CASES ##################################################################
+
+class TestMain(unittest.TestCase):
+
+    def testMain(self):
+        try:
+            fc = main.launch()
+            fc.close()
+        except Exception as e:
+            self.fail("Failed with the following exception: {}".format(e))
+            traceback.print_exc()
